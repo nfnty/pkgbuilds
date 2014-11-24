f446c478eed3d74315b7a41f0acdeac1b82b2f0e
nfnty <git@nfnty.se>
2014-11-24 17:29:11 +0100

    fixed script

--- rust/build.sh
+++ rust/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-DATE=`date --iso-8601 | sed 's/-/./g'`
+DATE=$(date --iso-8601 | sed 's/-/./g')
 
-sed -i "s/^pkgver=.*$/pkgver=$DATE/" 'rust-nightly-bin/PKGBUILD'
-sed -i "s/^pkgver=.*$/pkgver=$DATE/" 'cargo-nightly-bin/PKGBUILD'
+sed -i "s/^pkgver=.*$/pkgver=0.13.0_$DATE/" 'rust-nightly-bin/PKGBUILD'
+sed -i "s/^pkgver=.*$/pkgver=0.0.1_$DATE/" 'cargo-nightly-bin/PKGBUILD'
 
 unset DATE
 
