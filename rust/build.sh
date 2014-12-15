884df7d662f85deb985068939964c998d8193805
nfnty <git@nfnty.se>
2014-12-15 08:25:23 +0100

    fixed problem where rust build script would not download new sources

--- rust/build.sh
+++ rust/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+rm '/data/software/packages/cache/src/rust-nightly-x86_64-unknown-linux-gnu.tar.gz'
+rm '/data/software/packages/cache/src/cargo-nightly-x86_64-unknown-linux-gnu.tar.gz'
+
 DATE=$(date --iso-8601 | sed 's/-/./g')
 
 sed -i "s/^pkgver=.*$/pkgver=0.13.0_$DATE/" 'rust-nightly-bin/PKGBUILD'
@@ -7,7 +10,7 @@ sed -i "s/^pkgver=.*$/pkgver=0.0.1_$DATE/" 'cargo-nightly-bin/PKGBUILD'
 
 unset DATE
 
-cd 'rust-nightly-bin'
-makepkg --noconfirm -s -r -L -i
-cd '../cargo-nightly-bin'
-makepkg --noconfirm -s -r -L -i
+cd '/data/software/packages/AUR/rust/rust-nightly-bin'
+makepkg --noconfirm --log --syncdeps --rmdeps --install
+cd '/data/software/packages/AUR/rust/cargo-nightly-bin'
+makepkg --noconfirm --log --syncdeps --rmdeps --install
