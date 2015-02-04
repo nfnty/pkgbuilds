db61e4d5b364556a356c199117454aaa7ee14f09
nfnty <git@nfnty.se>
2015-02-04 08:17:28 +0100

    fixed rust/cargo

--- rust/build.sh
+++ rust/build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 rm '/data/software/packages/cache/src/rust-nightly-x86_64-unknown-linux-gnu.tar.gz'
-rm '/data/software/packages/cache/src/cargo-nightly-x86_64-unknown-linux-gnu.tar.gz'
 
 DATE=$(date --iso-8601 | sed 's/-/./g')
 
-sed -i "s/^pkgver=.*$/pkgver=0.13.0_$DATE/" 'rust-nightly-bin/PKGBUILD'
-sed -i "s/^pkgver=.*$/pkgver=0.0.1_$DATE/" 'cargo-nightly-bin/PKGBUILD'
+sed -i "s/^pkgver=.*$/pkgver=1.0.0_$DATE/" 'rust-nightly-bin/PKGBUILD'
 
 unset DATE
 
-cd '/data/software/packages/AUR/rust/rust-nightly-bin'
-makepkg --noconfirm --log --syncdeps --rmdeps --install
-cd '/data/software/packages/AUR/rust/cargo-nightly-bin'
-makepkg --noconfirm --log --syncdeps --rmdeps --install
+cd '/data/software/packages/builds/rust/rust-nightly-bin'
+makepkg --noconfirm --log --syncdeps --rmdeps --install --cleanbuild
