4cab645b8b721f00065f98bd2ceacd71a6d01921
nfnty <git@nfnty.se>
2016-02-21 10:44:59 +0100

    Rename install scripts

---
+++ rsyslog/install.sh
@@ -0,0 +1,9 @@
+post_install() {
+    groupadd --gid 534 rsyslog >/dev/null
+    useradd --uid 534 --gid heka --home-dir / --shell /usr/bin/false rsyslog >/dev/null
+}
+
+post_remove() {
+    userdel rsyslog &>/dev/null
+    groupdel rsyslog &>/dev/null
+}
