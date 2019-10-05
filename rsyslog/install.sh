8ff5ba767ab58a8e0c7dd5112650fa422268aa09
nfnty <git@nfnty.se>
2019-10-05 09:15:18 +0100

    Updated

--- rsyslog/install.sh
+++
@@ -1,8 +0,0 @@
-post_install() {
-    groupadd --gid 534 rsyslog >/dev/null
-    useradd --uid 534 --gid rsyslog --home-dir / --shell /usr/bin/false rsyslog >/dev/null
-}
-
-post_remove() {
-    userdel rsyslog >/dev/null
-}
